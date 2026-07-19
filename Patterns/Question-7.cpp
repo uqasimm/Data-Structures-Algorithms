544656b103d80e27dccaadab800d065dadc32be9
uqasimm <muhammadqasim2k07@gmail.com>
2026-07-19 20:19:09 +0500

    Problem #07 Solved

---
+++ Patterns/Question-7.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void printPattern(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            cout << " ";
+        }
+        for (int k = 0; k < (2 * i + 1); k++)
+        {
+            cout << "*";
+        }
+        for (int l = 0; l < n - i - 1; l++)
+        {
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    printPattern(n);
+    
+    return 0;
+}
