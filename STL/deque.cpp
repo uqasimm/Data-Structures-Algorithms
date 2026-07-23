c7a2da635b9da42cb2a009413db56546025375fe
uqasimm <muhammadqasim2k07@gmail.com>
2026-07-23 08:24:35 +0500

    Lists Complete, Deque Started

---
+++ STL/deque.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void explainDeque(){
+
+}
+
+int main(){
+    explainDeque();
+    return 0;
+}
---
+++ STL/lists.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Lists are same as vectors but lists also provide front operations as well.
+
+// Lists are doubly linked lists, whereas vectors are dynamic arrays
+
+void explainList(){
+    list<int> ls;
+    ls.push_back(2);
+
+    ls.emplace_back(2);
+
+    ls.insert(ls.begin(), 3, 10);
+    
+    ls.push_front(5);
+    
+    ls.emplace_front(1);
+    
+    for (auto i : ls)
+    {
+        cout << i << endl;
+    }
+}
+
+int main(){
+    explainList();
+    return 0;
+}
